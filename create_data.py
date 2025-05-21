852562c0e38701243f869d6e394b149c9bc85704
Natasha-R <natasha.j.randall@gmail.com>
2025-05-21 11:05:32 +0200

    Combining Sentinel 2 AOIs into rasters

--- create_data.py
+++ create_data.py
@@ -2,9 +2,10 @@ from data_processing.process_cems import create_cems_geojson, create_cems_raster
 from data_processing.create_metadata import find_aoi_extents, find_raster_extents
 from data_processing.modify_cems_permanent_water import create_permanent_water_geojson, create_permanent_water_raster, combine_cems_and_permanent_water
 from data_processing.create_dem import create_dem_rasters
-from data_processing.create_sentinel2 import find_sentinel2_availability, find_minimal_cloud_cover, download_sentinel2
+from data_processing.create_sentinel2 import find_sentinel2_availability, find_minimal_cloud_cover, download_sentinel2, create_sentinel2_rasters
 from data_processing.create_soil_moisture import create_soil_moisture_rasters
 
+import os
 import argparse
 
 ##### PRELIMINARY SET-UP
@@ -13,7 +14,7 @@ import argparse
 # 3. Download the seas and waters polygons from https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip, and place the unzipped folder into "water-polygons-split-4326" in "data_folder".
 
 def main(data_folder, fabdem_folder):
-    
+
     # Process the CEMS data
     create_cems_geojson(data_folder)
     create_cems_raster(data_folder)
@@ -34,6 +35,7 @@ def main(data_folder, fabdem_folder):
     find_sentinel2_availability(data_folder)
     find_minimal_cloud_cover(data_folder)
     download_sentinel2(data_folder)
+    create_sentinel2_rasters(data_folder)
 
     # Create the soil moisture rasters
     create_soil_moisture_rasters(data_folder)
@@ -43,9 +45,16 @@ def main(data_folder, fabdem_folder):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Create the full data stack for all CEMS data stored in 'data_folder'.")
-    parser.add_argument("--fabdem_folder", default=None, help="The path to the folder containing the full downloaded FABDEM.")
+    parser.add_argument("--fabdem_folder", required=True, help="The path to the folder containing the full downloaded FABDEM.")
     parser.add_argument("--data_folder", required=True, help="The path to the data folder.")
     
     args = parser.parse_args()
+
+    if not os.path.isdir(f"{args.data_folder}/raw_cems"):
+        raise FileNotFoundError("CEMS labels must be put in the raw_cems folder in", args.data_folder)
+    if len(os.listdir(f"{args.data_folder}/raw_cems"))==0:
+        raise FileNotFoundError("raw_cems folder is empty")
+    if len([file for file in os.listdir(args.fabdem_folder) if file.endswith(".zip")]) > 0:
+        raise ValueError("All image files must be extracted from FABDEM")
 
     main(data_folder=args.data_folder, fabdem_folder=args.fabdem_folder)
