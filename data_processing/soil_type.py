ebddcbac35b81d94fad793c3629e371d756036bc
Natasha-R <natasha.j.randall@gmail.com>
2025-07-17 15:28:00 +0200

    Add imputation for soil bulk density data

--- data_processing/soil_type.py
+++ data_processing/soil_type.py
@@ -47,10 +47,20 @@ def create_soil_type(data_folder, global_folder):
 
             # extract the given raster extent from the global raster
             resample_alg = "bilinear" if layer_name=="bulk_density" else "nearest"
-            gdal.Warp(soil_type_raster_path, global_path, 
-                      srcSRS="EPSG:4326", dstSRS="EPSG:4326", format='GTiff',
-                      resampleAlg=resample_alg, width=width, height=height, outputBounds=bounds)
-            
+            # for the bulk density data, impute the missing values using gdal_fillnodata
+            if layer_name == "bulk_density":
+                gdal.Warp(soil_type_raster_path, global_path, 
+                        srcSRS="EPSG:4326", dstSRS="EPSG:4326", format='GTiff',
+                        resampleAlg=resample_alg, outputBounds=bounds)
+                os.system(f"gdal_fillnodata -md=0 -q {soil_type_raster_path} {soil_type_raster_path}")
+                gdal.Warp(soil_type_raster_path, soil_type_raster_path, 
+                            srcSRS="EPSG:4326", dstSRS="EPSG:4326", format='GTiff',
+                            resampleAlg=resample_alg, width=width, height=height, outputBounds=bounds)
+            else:
+                gdal.Warp(soil_type_raster_path, global_path, 
+                        srcSRS="EPSG:4326", dstSRS="EPSG:4326", format='GTiff',
+                        resampleAlg=resample_alg, width=width, height=height, outputBounds=bounds)
+                
             # keep only data within the AOI bounds and fix the noData value
             with rasterio.open(soil_type_raster_path) as soil_type_file:
                 soil_type_raster = soil_type_file.read(1)
@@ -60,22 +70,23 @@ def create_soil_type(data_folder, global_folder):
                 soil_type_raster = np.where(soil_type_raster == 255, 254, soil_type_raster)
                 soil_type_raster = soil_type_raster + 1
                 soil_type_raster = np.where(soil_type_raster == 255, 0, soil_type_raster)
-            else:
-                soil_type_raster = np.where(soil_type_raster == -32768, 0, soil_type_raster)
             soil_type_raster = np.where(reference_label == 0, 0, soil_type_raster)
 
             # save the final raster
             with rasterio.open(soil_type_raster_path, "w", **meta, compress="LZW") as file:
                 file.write(soil_type_raster, 1)
                 file.set_band_description(1, f"soil_{layer_name}")
+                file.nodata = None
 
+            if os.path.exists(f"{soil_type_raster_path}.aux.xml"):
+                os.remove(f"{soil_type_raster_path}.aux.xml")
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Create rasters for soil classes and soil bulk density data.")
     
-    parser.add_argument("--data_folder", required=True, help="The path to the data folder.")
-    parser.add_argument("--global_folder", required=True, help="The path to the folder containing the global data")
+    parser.add_argument("--data_folder", default=os.environ["DATA_FOLDER"], help="The path to the data folder.")
+    parser.add_argument("--global_folder", default=os.environ["GLOBAL_FOLDER"], help="The path to the folder containing the global data")
 
     args = parser.parse_args()
 
