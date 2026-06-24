63fb9e24b421ff3283aba0ccd6bde43124d8e710
Natasha-R <natasha.j.randall@gmail.com>
2026-06-24 11:15:11 +0200

    Option to compute losses when training

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def setup(rank, world_size):
 def cleanup():
     dist.destroy_process_group()
 
-def train(rank, world_size, config_path, data_folder, modelling_folder, ddp=False):
+def train(rank, world_size, config_path, data_folder, modelling_folder, pretrained_path, compute_losses=False, ddp=False):
 
     if ddp:
         setup(rank, world_size)
@@ -34,10 +34,10 @@ def train(rank, world_size, config_path, data_folder, modelling_folder, ddp=Fals
     try:
         logger = utils.get_logger()
         config, config_name = utils.load_config(config_path, logger)
-        model = utils.load_model(config, rank, ddp, logger)
+        model = utils.load_model(config, rank, ddp, logger, pretrained_path)
         subsets = list(pd.read_csv(f"{data_folder}/subsets/{config['data_subset_file']}.csv")["subset"].unique())
         subsets = [subset for subset in subsets if "test" not in subset]
-        data_loaders = {subset: data_pipeline.create_data_loader(config, data_folder, ddp, subset) for subset in subsets}
+        data_loaders = {subset: data_pipeline.create_data_loader(config, data_folder, ddp, subset, training=True) for subset in subsets}
         if config.get("loss_function", "cross entropy").lower()=="dice":
             loss_function = DiceLoss(mode="binary", from_logits=True, ignore_index=0)
         else:
@@ -82,41 +82,46 @@ def train(rank, world_size, config_path, data_folder, modelling_folder, ddp=Fals
                 losses["epoch_train_local_losses"].append(local_losses)
                 if use_scheduler:
                     scheduler.step()
-                
-            model.eval()
-            with torch.no_grad():
-                for validation_loader in [val_set for val_set in subsets if "val" in val_set]:
-                    for data in tqdm(data_loaders[validation_loader], desc=f"Validation ({validation_loader}) loss", leave=False):
-                        for item in data.keys():
-                            data[item] = data[item].to(rank) #BCHW (features) #BHW (label)
-                        model_output = model(data)
-                        loss = 0.0
-                        local_losses = 0.0
-                        for scale in config["output_scales"]:
-                            indiv_loss = loss_function(model_output[f"{scale}_pred"], data[f"{scale}_label"])
-                            loss = loss + config[f"{scale}_weight"] * indiv_loss
-                            if scale == "local":
-                                local_losses += indiv_loss.item()
-                        losses[f"epoch_{validation_loader}_losses"].append(loss.item())
-                        losses[f"epoch_{validation_loader}_local_losses"].append(local_losses)
+            
+            if compute_losses:
+                model.eval()
+                with torch.no_grad():
+                    for validation_loader in [val_set for val_set in subsets if "val" in val_set]:
+                        for data in tqdm(data_loaders[validation_loader], desc=f"Validation ({validation_loader}) loss", leave=False):
+                            for item in data.keys():
+                                data[item] = data[item].to(rank) #BCHW (features) #BHW (label)
+                            model_output = model(data)
+                            loss = 0.0
+                            local_losses = 0.0
+                            for scale in config["output_scales"]:
+                                indiv_loss = loss_function(model_output[f"{scale}_pred"], data[f"{scale}_label"])
+                                loss = loss + config[f"{scale}_weight"] * indiv_loss
+                                if scale == "local":
+                                    local_losses += indiv_loss.item()
+                            losses[f"epoch_{validation_loader}_losses"].append(loss.item())
+                            losses[f"epoch_{validation_loader}_local_losses"].append(local_losses)
 
             loss_string = f"GPU: {rank} | Epoch {epoch} ({time.time() - epoch_start_time:.0f} seconds)"
-            for loss_type in subsets:
+            loss_subsets = subsets if compute_losses else ["train"]
+            for loss_type in loss_subsets:
                 losses[f"total_{loss_type}_losses"].append(sum(losses[f"epoch_{loss_type}_losses"]) / len(losses[f"epoch_{loss_type}_losses"]))
                 loss_string += f" | {loss_type} loss: {losses[f'total_{loss_type}_losses'][-1]:.3f}"
                 losses[f"total_{loss_type}_local_losses"].append(sum(losses[f"epoch_{loss_type}_local_losses"]) / len(losses[f"epoch_{loss_type}_local_losses"]))
                 loss_string += f" | {loss_type} local loss: {losses[f'total_{loss_type}_local_losses'][-1]:.3f}"
             logger.info(loss_string)
 
-            if config["save_model_on_epoch"] != 0 and ((epoch % config["save_model_on_epoch"]) == 0):
-                if rank==0 or ddp==False:
-                    model_save_path = f"{modelling_folder}/models/{config_name}_{epoch}.pth"
-                    torch.save(model.module.state_dict() if ddp else model.state_dict(), model_save_path)
-                    logger.info(f"Saved the model to: {model_save_path}")
+            if (config["save_model_on_epoch"] != 0) and ((epoch % config["save_model_on_epoch"]) == 0) and ((rank==0) or (ddp==False)):
+
+                model_save_path = f"{modelling_folder}/models/{config_name}_{epoch}.pth"
+                torch.save(model.module.state_dict() if ddp else model.state_dict(), model_save_path)
+                logger.info(f"Saved the model to: {model_save_path}")
+
+                if compute_losses:
                     plot_losses(losses, config, config_name, modelling_folder, rank, logger)
-                    for validation_loader in [val_set for val_set in subsets if "val" in val_set]:
-                        calculate_metrics(config, config_name, model, data_loaders[validation_loader], modelling_folder, epoch=epoch, device=rank, subset=validation_loader)
-                        logger.info(f"Saved {validation_loader} evaluation metrics to: {modelling_folder}/metrics/{config_name}.csv")
+
+                for validation_loader in [val_set for val_set in subsets if "val" in val_set]:
+                    calculate_metrics(config, config_name, model, data_loaders[validation_loader], modelling_folder, epoch=epoch, device=rank, subset=validation_loader)
+                    logger.info(f"Saved {validation_loader} evaluation metrics to: {modelling_folder}/metrics/{config_name}.csv")
     finally:
         if ddp:
             cleanup()
@@ -127,6 +132,8 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--data_folder', default=os.environ["DATA_FOLDER"], help="The path to the dataset folder.")
     parser.add_argument('-m', '--modelling_folder', default=os.environ["MODELLING_FOLDER"], help="The path to the modelling folder.")
     parser.add_argument('-g', '--gpu', default="ddp", help="Specify which gpu to train on. 'ddp' for parallel training, '0', '1' for a GPU, and 'cpu' for CPU.")
+    parser.add_argument('-p', '--pretrained', default=False, help="A path to pretrained model weights.")
+    parser.add_argument('-l', '--losses', action="store_true", default=False, help="Computes the losses after every epoch, if turned on.")
 
     args = parser.parse_args()
 
@@ -136,6 +143,7 @@ if __name__ == "__main__":
     
     world_size = torch.cuda.device_count()
     if args.gpu=="ddp":
-        mp.spawn(train, args=(world_size, args.config_path, args.data_folder, args.modelling_folder, True), nprocs=world_size, join=True)
+        mp.spawn(train, args=(world_size, args.config_path, args.data_folder, args.modelling_folder, args.pretrained, args.losses, True), nprocs=world_size, join=True)
     else:
-        train(rank=args.gpu, world_size=world_size, config_path=args.config_path, data_folder=args.data_folder, modelling_folder=args.modelling_folder, ddp=False)
+        train(rank=args.gpu, world_size=world_size, config_path=args.config_path, data_folder=args.data_folder, 
+              modelling_folder=args.modelling_folder, pretrained_path=args.pretrained, compute_losses=args.losses, ddp=False)
